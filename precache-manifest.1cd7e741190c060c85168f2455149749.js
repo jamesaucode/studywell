d176f17e6cf8ec8161d1e9c60cf049177af0e2db
James Au <jamesaubudi@gmail.com>
2018-12-26 00:02:59 -0500

    Updates

---
+++ precache-manifest.1cd7e741190c060c85168f2455149749.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "058bdd53e90aae2449bb",
+    "url": "/studywell/static/js/runtime~main.058bdd53.js"
+  },
+  {
+    "revision": "73c5a898224b5a6dc5a7",
+    "url": "/studywell/static/js/main.73c5a898.chunk.js"
+  },
+  {
+    "revision": "5db1da23b91c42607fae",
+    "url": "/studywell/static/js/1.5db1da23.chunk.js"
+  },
+  {
+    "revision": "73c5a898224b5a6dc5a7",
+    "url": "/studywell/static/css/main.529031f1.chunk.css"
+  },
+  {
+    "revision": "308e0346391ce82c51b6ded288bdfa72",
+    "url": "/studywell/index.html"
+  }
+];
